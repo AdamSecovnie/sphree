af3f82a8ff804a867e080e761a83ce46778564ed
Adam Secovnie <adamsecovnie@eawelectro.com>
2020-05-14 23:56:54 -0400

    Initial commit

---
+++ Assets/ShapeGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGenerator 
+{
+    ShapeSettings settings;
+
+    public ShapeGenerator(ShapeSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
+    {
+        //Debug.Log("Calculate Point On Planet: ["+ pointOnUnitSphere * settings.planetRadius +"]");
+        return pointOnUnitSphere * settings.planetRadius;
+    }
+}
